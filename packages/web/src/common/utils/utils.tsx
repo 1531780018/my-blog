bbca5ea3e3045dcf568c92b3f042ea5ae497b3d3
魔王 <1531780018@qq.com>
2023-03-01 19:56:13 +0800

    处理不需要的

--- packages/web/src/common/utils/utils.tsx
+++ packages/web/src/common/utils/utils.tsx
@@ -9,12 +9,7 @@
 import { lazy } from "react"
 import DocumentTitle from 'react-document-title'
 
-const HomeTitle = process.env.VITE_APP_TITLE
-
-export const LazyLoad = (to: string, title: string) => {
-  const Comp = lazy(async () => await import(`../../views/${to}`))
-  return (<DocumentTitle title={`${HomeTitle} - ${title}`}><Comp /></DocumentTitle>)
-}
+// const HomeTitle = process.env.VITE_APP_TITLE
 
 export const GetDescContext = (context: string) => {
 
