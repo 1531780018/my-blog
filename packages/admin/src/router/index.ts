8820302a123dc283569b40b8f7d10cf27b1b27f7
魔王 <1531780018@qq.com>
2023-03-07 14:30:44 +0800

    页面修改 编写权限

--- packages/admin/src/router/index.ts
+++ packages/admin/src/router/index.ts
@@ -2,19 +2,23 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
   {
-    path: '/',
+    path: '/admin',
     component: () => import("@/views/Index/index.vue"),
     children: [
       {
-        path: '/editPost',
+        path: '/admin/editPost',
         component: () => import("@/views/Post/postEdit.vue"),
       },
       {
-        path: '/postManage',
+        path: '/admin/postManage',
         component: () => import("@/views/Post/postManage.vue"),
       }
     ]
-  }
+  },
+  {
+    path: '/',
+    component: () => import("@/views/Main/index.vue"),
+  },
 ]
 
 export default createRouter({
---
+++ packages/admin/src/views/Main/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login">
+    <div class="login-content">
+      <div class="content">
+        <n-card hoverable>
+          <div class="login-form">
+            <n-form ref="formRef" :model="state.model" :rules="state.rules">
+              <n-form-item path="age" label="用户名">
+                <n-input v-model:value="state.model.username" @keydown.enter.prevent />
+              </n-form-item>
+              <n-form-item path="password" label="密码">
+                <n-input v-model:value="state.model.password" type="password" @keydown.enter.prevent />
+              </n-form-item>
+              <div>
+                <n-button type="info" style="width:100%"> 登录 </n-button>
+              </div>
+            </n-form>
+          </div>
+        </n-card>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="tsx" setup>
+import { reactive } from 'vue';
+const state = reactive({
+  model: {
+    username: "",
+    password: ""
+  },
+  rules: {
+
+  }
+})
+</script>
+<style lang="scss" scoped>
+.login {
+  background-image: url(@/assets/bg.jpeg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+
+  .login-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+    .content {
+      width: 400px;
+      padding: 30px 20px;
+    }
+  }
+
+  .login-form {}
+}
+</style>
